365026f7efaa27bb947df8dff26f31b9199bd68f
Your Name <you@example.com>
2025-04-24 18:20:09 -0300

    primer commit !

---
+++ src/clasecero/ClaseCero.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clasecero;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Matias
+ */
+public class ClaseCero {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    sc.nextDouble();
+        
+    }
+    
+}
